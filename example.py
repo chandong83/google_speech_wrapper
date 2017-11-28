f14fe695817dfe688703f60f0460e49935065af1
chandong83 <chandong83@naver.com>
2017-11-28 20:51:09 +0900

    first

---
+++ example.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import gspeech
+import time
+def main():
+    gsp = gspeech.Gspeech()
+    while True:
+        # 음성 인식 될때까지 대기 한다.
+        stt = gsp.getText()
+        if stt is None:
+            break
+        print(stt)
+        time.sleep(0.01)
+        if ('끝내자' in stt):
+            break
+
+
+if __name__ == '__main__':
+    main()
